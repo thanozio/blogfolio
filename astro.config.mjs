0eb34925eac45a54a669ac94f8a9ed80371ab74c
Thanozio <thanozio@proton.me>
2025-03-10 22:05:41 +0200

    feat: link tags and RSS support

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  devToolbar: {
-    enabled: false
-  }
+  site: "https://thanozio.netlify.app/"
 });
