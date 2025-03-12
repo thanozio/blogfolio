037c7012194f8a03cad92b68a3fc4855a6c7ce94
Thanozio <thanozio@proton.me>
2025-03-12 12:23:37 +0200

    refactor: css and styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,5 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://thanozio.netlify.app/",
+  devToolbar: { enabled: false },
 });
